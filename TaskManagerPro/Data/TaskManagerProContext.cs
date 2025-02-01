fdbba3defb586df0adeed5b74c2626e219621cf3
ivpehar <ivan.pehar@vsite.hr>
2025-02-01 21:41:48 +0100

    Login/Register

---
+++ TaskManagerPro/Data/TaskManagerProContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerPro.Data;
+
+namespace TaskManagerPro.Data
+{
+    public class TaskManagerProContext(DbContextOptions<TaskManagerProContext> options) : IdentityDbContext<User>(options)
+    {
+    }
+}
--- TaskManagerPro/Program.cs
+++ TaskManagerPro/Program.cs
@@ -1,10 +1,40 @@
-using TaskManagerPro.Components;
+﻿using TaskManagerPro.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerPro.Components.Account;
+using TaskManagerPro.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TaskManagerProContextConnection") ?? throw new InvalidOperationException("Connection string 'TaskManagerProContextConnection' not found.");;
+
+builder.Services.AddDbContext<TaskManagerProContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddScoped<IdentityUserAccessor>();
+
+builder.Services.AddScoped<IdentityRedirectManager>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+builder.Services.AddIdentityCore<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<TaskManagerProContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<User>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
@@ -23,5 +53,7 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapAdditionalIdentityEndpoints();;
 
 app.Run();
